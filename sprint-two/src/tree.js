9f522b6ddf2fe75af576891158db720f5a018b2f
cdcjj <chihyundorischiu@gmail.com>
2017-03-23 15:09:33 -0500

    finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,7 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = [];  // fix me
+  newTree.children = [];  // fixed
 
   return newTree;
 };
@@ -13,29 +13,23 @@ var treeMethods = {};
 treeMethods.addChild = function(value) {
   //recursively call Tree function
   var node = Tree(value);
-
-  debugger;
-  //if tree is empty;
-  if (this.children.length === 0) {
-    this.children.push(node);
-    console.log(this);
-    console.log(this.children);
-  } else {
-    debugger;
-    this.children[0].children.push(node);
-    // if there is children-- enter while loop.
-    // while (this.children) {
-      // keep looking for new children for children == null.
-
-    // }
-  }
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target) {
-  if (this.children[0].value === target) {
-    return true;
-  }
-  return false;
+  var result = false;
+  var searchTarget = function(node) {
+    if (node.value === target) {
+      result = true;
+    }
+
+    node.children.forEach(function (child) {
+      return searchTarget(child);
+    });
+  };
+  
+  searchTarget(this);
+  return result;
 };
 
 
